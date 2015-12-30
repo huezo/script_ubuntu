ad4ccb95ffa7006fefa10104cc6cf9d853f7d1a3
Huezo <huezohuezo.1990@gmail.com>
2015-12-30 00:12:23 -0600

    Create huezo.sh

---
+++ huezo.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#SCRIPT PARA ACTUALIZAR UBUNTU 
+#permisos en la terminal
+#chmod +x bash.sh
+#ejecutandola en la terminal
+#./bash.sh
+
+sudo apt-get update && sudo apt-get upgrade -y
+sudo apt-get update && sudo apt-get upgrade
+
+#esto me sirve para hacer un
+#Read Key con la cual no se me cerrara la terminal
+read -n1 -r -p "Presione Tecla espacio/espaciadora  para continuar..." key
+
+if [ "$key" = ' ' ]; then
+    # Si presiono espacio
+else
+    # SiNo presiono espacio
+fi
+
+
+
+#con esto hacemos que no se cierre
+#la terminal poniendo read 
+read
+read
